4dd3bf7df2005971a37e562348dfc44150eb3179
Loïc Mahieu <mahieuloic@gmail.com>
2014-07-01 12:26:20 +0200

    Test case for #1

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var isJSON = require('../');
 
 
 test('performe isJSON verifications', function (t) {
-  t.plan(10);
+  t.plan(11);
 
   t.deepEqual(isJSON(null), false, '`null`, should return false');
   t.deepEqual(isJSON(false), false, '`false`, should return false');
@@ -17,4 +17,5 @@ test('performe isJSON verifications', function (t) {
               'a JSON object `{a: 12, b: [1,2,3]}` but pass the 2 arg as true (check objects too), should return true');
   t.deepEqual(isJSON('{"a":"obja","b":[0,1,2],"c":{"d":"some object"}}'), true,
               '`{"a":"obja","b":[0,1,2],"c":{"d":"some object"}}`, should return true');
+  t.deepEqual(isJSON('1,2,3'), false, '`1,2,3`, should return false');
 });
